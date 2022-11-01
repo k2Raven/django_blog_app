c64d47f92f5cb4f0002c36c26b5949473c4ae4c7
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2022-11-01 21:25:26 +0600

    Результат занятия 45

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
+from webapp.models import Article
 
 
 def index_views(request):
-    return render(request, "index.html")
+    articles = Article.objects.order_by('-created_at')
+    context = {
+        'articles': articles
+    }
+    return render(request, "index.html", context)
+
+
+def article_view(request):
+    article_id = request.GET.get('id')
+    article = Article.objects.get(pk=article_id)
+    context = {'article': article}
+    return render(request, 'article_view.html', context)
 
 
 def article_create_view(request):
     if request.method == "GET":
         return render(request, "article_create.html")
     elif request.method == "POST":
-        context = {
-            'title': request.POST.get('title'),
-            'content': request.POST.get('content'),
-            'author': request.POST.get('author')
-        }
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        author = request.POST.get('author')
+        new_article = Article.objects.create(title=title, content=content, author=author)
+        context = {'article': new_article}
         return render(request, 'article_view.html', context)
