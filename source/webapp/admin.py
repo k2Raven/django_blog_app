68fedc0bce511cab47aa7783697f91e8a28d64f1
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2022-11-01 20:46:19 +0600

    создали модель Article, создали мегарации, зарегестрировали в панели администратора

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from webapp.models import Article
+
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'created_at']
+    list_display_links = ['title']
+    list_filter = ['author']
+    search_fields = ['title', 'content']
+    # fields = ['id', 'title', 'author', 'content', 'created_at', 'updated_at']
+    exclude = []
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Article, ArticleAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Заголовок")
+    author = models.CharField(max_length=50, verbose_name="Автор", default="Unknown")
+    content = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Контент")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+
+    def __str__(self):
+        return f'{self.pk}. {self.title}'
